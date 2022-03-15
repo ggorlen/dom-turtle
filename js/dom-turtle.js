fd70ed403ecd3f11596edaa9c6e92b0d7e8050cf
ggorlen <gsgorlen@gmail.com>
2022-03-14 17:39:23 -0700

    use object prototype

--- js/dom-turtle.js
+++ js/dom-turtle.js
@@ -5,82 +5,81 @@
     return deg * Math.PI / 180;
   }
 
-  function turtle(rootElement) {
-    return {
-      rootElement: rootElement,
-      x: 0,
-      y: 0,
-      angle: 0,
-      html: '<input type="radio" name="turtle" checked>',
-      stamps: [],
-      heading: function () {
-        return this.angle;
-      },
-      setheading: function (angle) {
-        this.angle = angle;
-      },
-      setx: function (x) {
-        this.x = x;
-      },
-      sety: function (y) {
-        this.y = y;
-      },
-      xcor: function () {
-        return this.x;
-      },
-      ycor: function () {
-        return this.y;
-      },
-      goto: function (x, y) {
-        this.x = x;
-        this.y = y;
-      },
-      forward: function (distance) {
-        this.x += Math.cos(rad(this.angle)) * distance;
-        this.y += Math.sin(rad(this.angle)) * distance;
-      },
-      backward: function (distance) {
-        this.x -= Math.cos(rad(this.angle)) * distance;
-        this.y -= Math.sin(rad(this.angle)) * distance;
-      },
-      left: function (angle) {
-        this.angle -= angle;
-      },
-      right: function (angle) {
-        this.angle += angle;
-      },
-      shape: function (html) {
-        this.html = html;
-      },
-      stamp: function () {
-        var el = document.createElement("div");
-        el.innerHTML = this.html;
-        rootElement.appendChild(el);
-        this.stamps.push(el);
-        el.classList.add("turtle-el");
-        el.style.left = this.x + "px";
-        el.style.top = this.y + "px";
-        return this.stamps.length - 1;
-      },
-      clearstamps: function () {
-        this.stamps.forEach(e => e.remove());
-        this.stamps = [];
-      },
-      clearstamp: function (stampid) {
-        this.stamps.splice(stampid, 1)[0].remove();
-      },
-      position: function () {
-        return [this.x, this.y];
-      },
-
-      getstamp: function (stampid) {
-        return this.stamps[stampid];
-      },
-    };
+  var turtle = function (rootElement) {
+    this.rootElement = rootElement;
+    this.x = 0;
+    this.y = 0;
+    this.angle = 0;
+    this.html = '<input type="radio" name="turtle" checked>';
+    this.stamps = [];
+  };
+  turtle.prototype.heading = function () {
+    return this.angle;
+  };
+  turtle.prototype.setheading = function (angle) {
+    this.angle = angle;
+  };
+  turtle.prototype.setx = function (x) {
+    this.x = x;
+  };
+  turtle.prototype.sety = function (y) {
+    this.y = y;
+  };
+  turtle.prototype.xcor = function () {
+    return this.x;
+  };
+  turtle.prototype.ycor = function () {
+    return this.y;
+  };
+  turtle.prototype.goto = function (x, y) {
+    this.x = x;
+    this.y = y;
+  };
+  turtle.prototype.forward = function (distance) {
+    this.x += Math.cos(rad(this.angle)) * distance;
+    this.y += Math.sin(rad(this.angle)) * distance;
+  };
+  turtle.prototype.backward = function (distance) {
+    this.x -= Math.cos(rad(this.angle)) * distance;
+    this.y -= Math.sin(rad(this.angle)) * distance;
+  };
+  turtle.prototype.left = function (angle) {
+    this.angle -= angle;
+  };
+  turtle.prototype.right = function (angle) {
+    this.angle += angle;
+  };
+  turtle.prototype.shape = function (html) {
+    this.html = html;
+  };
+  turtle.prototype.stamp = function () {
+    var el = document.createElement("div");
+    el.innerHTML = this.html;
+    this.rootElement.appendChild(el);
+    this.stamps.push(el);
+    el.classList.add("turtle-el");
+    el.style.left = this.x + "px";
+    el.style.top = this.y + "px";
+    return this.stamps.length - 1;
+  };
+  turtle.prototype.clearstamps = function () {
+    this.stamps.forEach(e => e.remove());
+    this.stamps = [];
+  };
+  turtle.prototype.clearstamp = function (stampid) {
+    this.stamps.splice(stampid, 1)[0].remove();
+  };
+  turtle.prototype.position = function () {
+    return [this.x, this.y];
+  };
+  turtle.prototype.getstamp = function (stampid) {
+    return this.stamps[stampid];
   };
 
   var Turtle = {
-    turtle: turtle,
+    turtle: function (rootElement) {
+      return new turtle(rootElement);
+    },
   };
 
   if (typeof module === "object" && 
